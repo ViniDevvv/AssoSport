bb824c3822c3a9645d0ead244992050f9af19913
Andrea LAUMONIER <LAUMONIER@bts.leo>
2026-03-11 15:25:18 +0100

    Commit 1

---
+++ NiceAssoSpot/resources/views/login.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Authentification</title>
+        
+    </head>
+<body>
+    <h1>Authentification</h1>
+    
+    <form>
+        <div>
+            <!--Champs de texte nom utilisateur -->
+            <label for="username">e-mail : </label>
+            <input type="text" id="email" name="name" />
+        </div>
+        <div>
+            <!--Champs de texte mot de passe -->
+            <label for="username">Mot de passe : </label>
+            <input type="text" id="mdp" name="mdp" />
+        </div>
+        <div>
+            <button>Envoyer</button>
+        </div>
+    </form>
+</body>
+</html>
